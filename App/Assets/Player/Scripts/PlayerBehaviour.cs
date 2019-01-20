e7781e555fecaf0a779ebb0ea698e2a41808524d
JulianCDC <32849409+JulianCDC@users.noreply.github.com>
2019-01-20 16:52:28 +0100

    + Player (visual)

--- App/Assets/Player/Scripts/PlayerBehaviour.cs
+++ App/Assets/Player/Scripts/PlayerBehaviour.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Behaviour : MonoBehaviour
+public class PlayerBehaviour : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
