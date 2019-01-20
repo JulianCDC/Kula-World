6799417c6a8c756747c685d08add3bd945ddffe8
JulianCDC <32849409+JulianCDC@users.noreply.github.com>
2019-01-20 19:35:42 +0100

    + Coin
    no texture yet

---
+++ App/Assets/Collectibles/Coin/Scripts/PieceBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceBehaviour : Collectible
+{
+    void Start()
+    {
+        
+    }
+
+    override protected void Update()
+    {
+        base.Update();
+    }
+
+    protected override void Collected()
+    {
+        // TODO : update score
+    }
+}
--- App/Assets/Scripts/Abstract/Collectible.cs
+++ App/Assets/Scripts/Abstract/Collectible.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 abstract public class Collectible : MonoBehaviour
 {
+    public enum rotationDirections { x, y, z };
+    public rotationDirections rotationDirection;
     abstract protected void Collected();
 
     virtual protected void Update()
     {
-        transform.Rotate(0, 50 * Time.deltaTime, 0);
+        float x = 0, y = 0, z = 0;
+        switch(this.rotationDirection)
+        {
+            case rotationDirections.x:
+                x = 50 * Time.deltaTime;
+                break;
+            case rotationDirections.y:
+                y = 50 * Time.deltaTime;
+                break;
+            case rotationDirections.z:
+                z = 50 * Time.deltaTime;
+                break;
+        }
+        transform.Rotate(x, y, z);
     }
 }
