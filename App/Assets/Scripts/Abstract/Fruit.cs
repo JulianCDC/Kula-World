49b3bb24b57568fbba2f754107320b0ffdcfa515
JulianCDC <32849409+JulianCDC@users.noreply.github.com>
2019-01-25 19:00:54 +0100

    + Fruits class

---
+++ App/Assets/Scripts/Abstract/Fruit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class Fruit : Collectible
+{
+    public enum fruits { apple, banana, watermelon, strawberry, pumpkin };
+    public fruits type;
+
+    protected override void Collected()
+    {
+        // collect type
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
