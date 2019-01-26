82c705c99b9042e6aaf02e13fa210ed04f64c394
JulianCDC <32849409+JulianCDC@users.noreply.github.com>
2019-01-26 20:19:24 +0100

    + Pills

---
+++ App/Assets/Collectibles/Pills/Scripts/Lethargy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lethargy : Collectible
+{
+    protected override void Collected()
+    {
+        // slow down ball and speed up timer
+    }
+}
