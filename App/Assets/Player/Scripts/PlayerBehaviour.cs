e2abaaff8189b0b2789ba530ad748de13f3154d6
JulianCDC <32849409+JulianCDC@users.noreply.github.com>
2019-01-26 20:31:35 +0100

    + PointCollectible : Collectible for gem and coins

--- App/Assets/Player/Scripts/PlayerBehaviour.cs
+++ App/Assets/Player/Scripts/PlayerBehaviour.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private int speed;
+    private short jumpLength;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
---
+++ App/Assets/Scripts/Abstract/PointCollectible.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointCollectible : Collectible
+{
+    public int value;
+
+    protected override void Collected()
+    {
+        // increment player score by value
+    }
+}
