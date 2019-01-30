74dc2f6c67775b3e182a846aabbdb7cc33fbd62e
JulianCDC <32849409+JulianCDC@users.noreply.github.com>
2019-01-30 06:59:36 +0100

    + Hourglass

---
+++ App/Assets/Collectibles/Hourglass/Scripts/HourglassBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HourglassBehaviour : Collectible
+{
+    protected override void Collected()
+    {
+        // add time to timer
+    }
+}
