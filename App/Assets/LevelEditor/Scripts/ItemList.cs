5c3129c8f46bf6c97364fcabb8ea3435ff3c0aa1
JulianCDC <julian.gardez@outlook.com>
2019-02-13 11:37:58 +0100

    Added preview images

--- App/Assets/LevelEditor/Scripts/ItemList.cs
+++ App/Assets/LevelEditor/Scripts/ItemList.cs
@@ -6,35 +6,20 @@ using UnityEngine.UI;
 
 public class ItemList : MonoBehaviour
 {
-    private GameObject[] prefabs;
+    private Texture2D[] textures;
     public Image previewTemplate;
 
     void Start()
     {
-        prefabs = Resources.LoadAll<GameObject>("EditorItemPrefab");
-        string position = "up";
+        textures = Resources.LoadAll<Texture2D>("EditorItemPreview");
 
-        foreach (GameObject prefab in prefabs)
+        foreach (Texture2D prefabTexture in textures)
         {
-            GameObject gameObject = Instantiate(prefab, this.gameObject.transform);
-
             Image image = Instantiate(previewTemplate, this.transform);
 
-            RuntimePreviewGenerator.TransparentBackground = true;
-            Sprite imageSprite = Sprite.Create(RuntimePreviewGenerator.GenerateModelPreview(gameObject.transform, 100, 100), new Rect(0, 0, 100, 100), new Vector2(0, 0));
+            Sprite imageSprite = Sprite.Create(prefabTexture, new Rect(0, 0, 128, 128), new Vector2(0, 0));
 
             image.sprite = imageSprite;
-
-            Texture2D texture = new Texture2D(100, 100, TextureFormat.RGB24, false);
-
-            texture.ReadPixels(image.sprite.rect, 0, 0);
-            texture.Apply();
-
-            byte[] bytes = texture.EncodeToPNG();
-            File.WriteAllBytes(Application.dataPath + "/" + prefab.name + "_" + position + ".png", bytes);
-
-            Destroy(gameObject);
-            Destroy(image);
         }
     }
 
