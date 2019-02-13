9fa255950ffd766cd30cc41c3c2bc5c3548d9e45
JulianCDC <julian.gardez@outlook.com>
2019-02-13 18:50:42 +0100

    added menubaritem prefab's script

---
+++ App/Assets/LevelEditor/Scripts/MenuBarButtonBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBarButtonBehaviour : MonoBehaviour
+{
+    public enum Type { action, scene };
+    public Type actionType;
+    public UnityEngine.SceneManagement.Scene actionScene;
+    public UnityEngine.Events.UnityEvent actionFunction;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ExecuteAction()
+    {
+        switch (actionType)
+        {
+            case Type.scene:
+                UnityEngine.SceneManagement.SceneManager.LoadScene(actionScene.buildIndex);
+                break;
+            case Type.action:
+                actionFunction.Invoke();
+                break;
+        }
+    }
+
+    // actions' functions
+}
