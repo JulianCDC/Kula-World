f4ea63517a0e137ed381cbff928227c63def86f9
JulianCDC <julian.gardez@outlook.com>
2019-02-13 12:20:57 +0100

    Preview blockwithitem in editor

--- App/Assets/Block/Scripts/WithItemBehaviour.cs
+++ App/Assets/Block/Scripts/WithItemBehaviour.cs
@@ -9,57 +9,66 @@ public class WithItemBehaviour : MonoBehaviour
     public Positions itemPosition;
     private GameObject childCollectible;
     private Collectible childBehaviour;
+    private float initialYaw;
+    private float initialPitch;
+    private float initialRoll;
 
-    void Awake()
+    public void Start()
     {
         this.childCollectible = this.transform.GetChild(0).gameObject;
         this.childBehaviour = (Collectible)this.childCollectible.GetComponent(typeof(Collectible));
 
         Quaternion rotation = childCollectible.transform.rotation;
+        this.initialYaw = rotation.eulerAngles.x;
+        this.initialPitch = rotation.eulerAngles.y;
+        this.initialRoll = rotation.eulerAngles.z;
+
+        setItemPosition();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void setItemPosition()
+    {
         Quaternion newRotation;
-        float yaw = rotation.eulerAngles.x;
-        float pitch = rotation.eulerAngles.y;
-        float roll = rotation.eulerAngles.z;
 
         switch (this.itemPosition)
         {
             default:
             case Positions.up:
                 this.childCollectible.transform.localPosition = new Vector3(0, 1, 0);
-                newRotation = Quaternion.Euler(yaw, pitch, roll);
+                newRotation = Quaternion.Euler(initialYaw, initialPitch, initialRoll);
                 this.childBehaviour.rotationDirection = Collectible.RotationDirections.y;
                 break;
             case Positions.right:
                 this.childCollectible.transform.localPosition = new Vector3(0, 0, 1);
-                newRotation = Quaternion.Euler(yaw + 60, pitch, roll - 90);
+                newRotation = Quaternion.Euler(initialYaw + 60, initialPitch, initialRoll - 90);
                 this.childBehaviour.rotationDirection = Collectible.RotationDirections.z;
                 break;
             case Positions.left:
                 this.childCollectible.transform.localPosition = new Vector3(0, 0, -1);
-                newRotation = Quaternion.Euler(yaw - 120, pitch, roll - 90);
+                newRotation = Quaternion.Euler(initialYaw - 120, initialPitch, initialRoll - 90);
                 this.childBehaviour.rotationDirection = Collectible.RotationDirections.z;
                 break;
             case Positions.down:
                 this.childCollectible.transform.localPosition = new Vector3(0, -1, 0);
-                newRotation = Quaternion.Euler(yaw + 150, pitch, roll - 90);
+                newRotation = Quaternion.Euler(initialYaw + 150, initialPitch, initialRoll - 90);
                 break;
             case Positions.front:
                 this.childCollectible.transform.localPosition = new Vector3(-1, 0, 0);
-                newRotation = Quaternion.Euler(yaw + 60, pitch - 60, roll - 270);
+                newRotation = Quaternion.Euler(initialYaw + 60, initialPitch - 60, initialRoll - 270);
                 this.childBehaviour.rotationDirection = Collectible.RotationDirections.z;
                 break;
             case Positions.back:
                 this.childCollectible.transform.localPosition = new Vector3(1, 0, 0);
-                newRotation = Quaternion.Euler(yaw + 60, pitch + 60, roll - 90);
+                newRotation = Quaternion.Euler(initialYaw + 60, initialPitch + 60, initialRoll - 90);
                 this.childBehaviour.rotationDirection = Collectible.RotationDirections.z;
                 break;
         }
 
         this.childCollectible.transform.localRotation = newRotation;
-    }
-
-    void Update()
-    {
-        
     }
 }
---
+++ App/Assets/Block/Scripts/WithItemEditorDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class WithItemEditorDisplay : MonoBehaviour
+{
+    public WithItemBehaviour script;
+
+    void Start()
+    {
+#if UNITY_EDITOR
+        script.Start();
+#endif
+    }
+
+    void Update()
+    {
+#if UNITY_EDITOR
+        script.setItemPosition();
+#endif
+    }
+}
