95aa6dbe4339d023066fc94aa06e9cb8b6089cb6
JulianCDC <julian.gardez@outlook.com>
2019-02-14 19:07:10 +0100

    Added possibility to select block in editor (incomplete)

--- App/Assets/LevelEditor/Scripts/EditableBlockBehaviour.cs
+++ App/Assets/LevelEditor/Scripts/EditableBlockBehaviour.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class EditableBlockBehaviour : MonoBehaviour
 {
+    private bool selected = false;
+
     void Start()
     {
         
@@ -12,10 +14,39 @@ public class EditableBlockBehaviour : MonoBehaviour
 
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit = new RaycastHit();
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject == this.gameObject && !this.selected)
+                {
+                    Select();
+                }
+
+                if (this.selected && hit.collider.gameObject != this.gameObject)
+                {
+                    UnSelect();
+                }
+            }
+            else
+            {
+                UnSelect();
+            }
+        }
     }
 
     public void Select()
     {
+        this.selected = true;
+        print("selected");
+    }
+
+    public void UnSelect()
+    {
+        this.selected = false;
+        print("unselected");
     }
 }
