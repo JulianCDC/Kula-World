362eb4c5c0e88f9a2f1c2fb0c07ed9c7daa925b0
Max <maxencegorez485@gmail.com>
2019-02-14 16:10:45 +0100

    Menu Structure + Menu
    prefebe du canvas, texte et bouton avec loadgroupe vertical centrale.

---
+++ App/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("ExempleScene");
+    }
+
+    public void RetrunMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MenuScene");
+    }
+}
