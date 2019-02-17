d9009e1730d28d020dec40b5bf96c7767a96fe2f
JulianCDC <julian.gardez@outlook.com>
2019-02-17 21:43:59 +0100

    Created map class with future methods

---
+++ App/Assets/Scenes/LevelEditor/Scripts/MapManipulation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class MapManipulation
+{
+    static void CreateMapFile()
+    {
+
+    }
+
+    static void MoveItem(int id, int newXPos, int newYPos, int newZPos)
+    {
+        CheckIfMoveIsPossible(id, newXPos, newYPos, newZPos);
+    }
+
+    static void CreateItem(string gameObjectName, int id, int x, int y, int z)
+    {
+
+    }
+
+    static void CreateItem(string gameObjectName, int id, int[] positions)
+    {
+        CreateItem(gameObjectName, id, positions[0], positions[1], positions[2]);
+    }
+
+    static void CheckIfMoveIsPossible(int id, int newXPos, int newYPos, int newZPos)
+    {
+
+    }
+}
