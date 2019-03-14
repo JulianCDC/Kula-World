d77cebcfbb5af570d7faac05852580aedc07c70e
JulianCDC <julian.gardez@outlook.com>
2019-03-14 17:40:28 +0100

    Added a ClickCapturer object for capturing click when displaying a popup

---
+++ App/Assets/Resources/ClickCapturer/Scripts/ClickCapturerBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class ClickCapturerBehaviour : MonoBehaviour
+{
+    public float Transparency
+    {
+        set
+        {
+            Image image = GetComponent<Image>();
+            Color imageColor = image.color;
+            imageColor.a = value;
+            image.color = imageColor;
+        }
+    }
+}
