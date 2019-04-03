14435a32fd05862b6258baa75bfdfb202aff07cd
JulianCDC <julian.gardez@outlook.com>
2019-04-03 09:22:11 +0200

    Added basic player movements

--- App/Assets/Player/Scripts/PlayerBehaviour.cs
+++ App/Assets/Player/Scripts/PlayerBehaviour.cs
@@ -9,22 +9,72 @@ public class PlayerBehaviour : MonoBehaviour
     /// <summary>
     /// The speed of the player
     /// </summary>
-    private int speed;
+    private float speed = 0.2f;
     /// <summary>
     /// The number of block the player will jump before touching the ground
     /// </summary>
     private short jumpLength = 1;
 
-    [NonSerialized] private bool isMoving = false;
+    private Vector3 positionBeforeMovement = Vector3.zero;
+    private bool isMoving;
+    private Vector3 movingDirection = Vector3.zero;
+    private Rigidbody rb;
 
     void Start()
     {
+        this.rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        if (!this.isMoving)
+        {
+            ListenForMovement();
+        }
+        else
+        {
+            Move();
+        }
     }
 
+    private void Move()
+    {
+        var position = this.gameObject.transform.position;
+        position = Vector3.MoveTowards(position, position + movingDirection, speed);
+        this.gameObject.transform.position = position;
+        if (this.transform.position == this.positionBeforeMovement + movingDirection)
+        {
+            this.isMoving = false;
+        }
+    }
+    
+    private void ListenForMovement()
+    {
+        if (Input.GetKeyDown("left"))
+        {
+            this.movingDirection = Vector3.right;
+        }
+        else if (Input.GetKeyDown("right"))
+        {
+            this.movingDirection = Vector3.left;
+        }
+        else if (Input.GetKeyDown("up"))
+        {
+            this.movingDirection = Vector3.back;
+        }
+        else if (Input.GetKeyDown("down"))
+        {
+            this.movingDirection = Vector3.forward;
+        }
+        else
+        {
+            return;
+        }
+
+        this.positionBeforeMovement = this.gameObject.transform.position;
+        this.isMoving = true;
+    }
+    
     public void FinishedMoving()
     {
         this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
