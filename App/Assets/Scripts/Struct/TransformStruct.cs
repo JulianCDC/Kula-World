ca641bb675b0a4b8236496850a8173eb6427c75a
Julian Gardez <julian.gardez@outlook.com>
2019-05-06 10:16:38 +0200

    :sparkles: Add class reproducing structure of Transform class

---
+++ App/Assets/Scripts/Struct/TransformStruct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class TransformStruct
+{
+    [Serializable]
+    public class TransformType
+    {
+        public float x;
+        public float y;
+        public float z;
+    }
+
+    [SerializeField] public TransformType position;
+    [SerializeField] public TransformType rotation;
+    [SerializeField] public TransformType scale;
+}
