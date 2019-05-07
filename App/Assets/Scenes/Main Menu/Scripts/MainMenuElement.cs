ca20c0f2a4045e4456ef24d43adc81a02d7c8fce
Julian Gardez <julian.gardez@outlook.com>
2019-05-07 09:31:09 +0200

    :lipstick: Change menu button styling

--- App/Assets/Scenes/Main Menu/Scripts/MainMenuElement.cs
+++ App/Assets/Scenes/Main Menu/Scripts/MainMenuElement.cs
@@ -1,11 +1,30 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 /// <summary>
 /// The Main Behaviour for the Main Menu Scene
 /// </summary>
-public class MainMenuElement : MonoBehaviour
+public class MainMenuElement : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    [SerializeField] private Text childText;
+    [SerializeField] private Color textHoverColor;
+    private Color oldTextColor;
+    
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        oldTextColor = childText.color;
+        childText.color = textHoverColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        childText.color = oldTextColor;
+    }
+    
     // list actions here
     
     /// <summary>
