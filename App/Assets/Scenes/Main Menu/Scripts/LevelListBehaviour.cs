b63cca5c50ba54ccfb2de440e08f420b2e888695
Julian Gardez <julian.gardez@outlook.com>
2019-05-07 12:42:22 +0200

    :sparkles: Load level names from custom map directory

---
+++ App/Assets/Scenes/Main Menu/Scripts/LevelListBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LevelListBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject listItem;
+    
+    void Start()
+    {
+        foreach (var file in Directory.GetFiles(Const.MAP_DIRECTORY, "*.map"))
+        {
+            var mapEntry = Instantiate(listItem, this.transform);
+            LevelListItemBehaviour mapEntryBehaviour = mapEntry.GetComponent<LevelListItemBehaviour>();
+
+            mapEntryBehaviour.MapName = Path.GetFileNameWithoutExtension(Path.Combine(Const.MAP_DIRECTORY, file));
+        }
+    }
+}
---
+++ App/Assets/Scenes/Main Menu/Scripts/LevelListItemBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelListItemBehaviour : MonoBehaviour
+{
+    private string _mapName;
+    [SerializeField] private Text label;
+
+    public string MapName
+    {
+        get { return _mapName; }
+        set
+        {
+            label.text = value;
+            _mapName = value;
+        }
+    }
+}
