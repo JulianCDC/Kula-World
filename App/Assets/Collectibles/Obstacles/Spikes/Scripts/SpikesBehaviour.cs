822b4e297bede2d12e5f121a35768798d9d0d4ce
Julian Gardez <julian.gardez@outlook.com>
2019-05-10 23:42:34 +0200

    :sparkles: Added spikes

---
+++ App/Assets/Collectibles/Obstacles/Spikes/Scripts/SpikesBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SpikesBehaviour : Collectible
+{
+    public bool permanent = true;
+    [SerializeField] private int idleTime = 3;
+    
+    protected override void Start()
+    {
+        base.Start();
+        
+        this.transform.Translate(Vector3.back * 0.42f);
+        if (!this.permanent)
+        {
+           // animate
+        }
+    }
+
+    public override void Collected()
+    {
+        // kill player
+    }
+}
