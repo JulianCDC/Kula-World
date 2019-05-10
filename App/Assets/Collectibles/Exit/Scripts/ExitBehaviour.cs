4fad7361df2886894baeaf307160c6ef2c07db3d
Julian Gardez <julian.gardez@outlook.com>
2019-05-10 21:25:34 +0200

    :sparkles: Added exit block

---
+++ App/Assets/Collectibles/Exit/Scripts/ExitBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitBehaviour : Collectible
+{
+    public override void Collected()
+    {
+        base.Collected();
+    }
+}
---
+++ App/Assets/Collectibles/Exit/Scripts/ExitPadBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ExitPadBehaviour : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(CycleColor());
+    }
+
+    private IEnumerator CycleColor()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        Color currentColor = renderer.material.color;
+        
+        while (true)
+        {
+            var t = Mathf.PingPong(Time.time, 1);
+            currentColor = Color.Lerp(Color.white, Color.blue, t);
+
+            renderer.material.color = currentColor;
+            
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
