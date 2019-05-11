f96cf8eff2a44227091d0395d349cbc00963e54f
Julian Gardez <julian.gardez@outlook.com>
2019-05-11 13:28:34 +0200

    :construction: added Block with spikes (not functional)

--- App/Assets/Collectibles/Obstacles/Spikes/Scripts/SpikesBehaviour.cs
+++ App/Assets/Collectibles/Obstacles/Spikes/Scripts/SpikesBehaviour.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 public class SpikesBehaviour : Collectible
 {
     public bool permanent = true;
-    [SerializeField] private int idleTime = 3;
-    
+    private static readonly int Permanent = Animator.StringToHash("Permanent");
+
     protected override void Start()
     {
         base.Start();
-        
+
+        print(transform.position);
         this.transform.Translate(Vector3.back * 0.42f);
-        if (!this.permanent)
-        {
-           // animate
-        }
+        print(transform.position);
+        GetComponent<Animator>().SetBool(Permanent, this.permanent);
     }
 
     public override void Collected()
