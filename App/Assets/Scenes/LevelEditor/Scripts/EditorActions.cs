024b0d20377fa5d0113c8fb4e6ceffb6dd69178e
Julian Gardez <julian.gardez@outlook.com>
2019-05-21 23:56:38 +0200

    :children_crossing: Revamped camera movement in editor

--- App/Assets/Scenes/LevelEditor/Scripts/EditorActions.cs
+++ App/Assets/Scenes/LevelEditor/Scripts/EditorActions.cs
@@ -8,23 +8,30 @@ using UnityEngine.EventSystems;
 public class EditorActions : MonoBehaviour
 {
     public Transform fovTransform;
+    [SerializeField] private Camera mainCamera;
     private GameObject currentlyPlacing;
-    
+
+    private float cameraYaw = 0;
+    private float cameraPitch = 0;
+
     private void Start()
     {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
         Map.mapInstance = new Map();
     }
-    
+
     void Update()
     {
         CameraControls();
         SelectListener();
         DeleteBlockListener();
         CancelListener();
-        
+
         if (EditorManager.Instance.newBlock != null && EditorManager.Instance.SelectedBlock == null)
         {
-            EditorManager.Instance.SelectedBlock = Instantiate(EditorManager.Instance.newBlock, Vector3.zero, Quaternion.Euler(Vector3.up * 90));
+            EditorManager.Instance.SelectedBlock = Instantiate(EditorManager.Instance.newBlock, Vector3.zero,
+                Quaternion.Euler(Vector3.up * 90));
             EditorManager.Instance.selectedBlockBehaviour.Select();
             EditorManager.Instance.SelectedBlock.transform.parent = GameObject.Find("Map").transform;
         }
@@ -67,7 +74,7 @@ public class EditorActions : MonoBehaviour
             }
         }
     }
-    
+
     private void ClearSelectedObject()
     {
         if (EditorManager.Instance.selectedBlockBehaviour != null)
@@ -78,7 +85,7 @@ public class EditorActions : MonoBehaviour
         EditorManager.Instance.SelectedBlock = null;
         EditorManager.Instance.selectedBlockBehaviour = null;
     }
-    
+
     private void DeleteBlockListener()
     {
         if (Input.GetKeyDown(KeyCode.Backspace) || Input.GetKeyDown(KeyCode.Delete))
@@ -99,119 +106,36 @@ public class EditorActions : MonoBehaviour
         }
     }
 
-    public void ManageTime( string userTime)
+    public void ManageTime(string userTime)
     {
         int time;
-        bool isInt =  int.TryParse(userTime, out time);
+        bool isInt = int.TryParse(userTime, out time);
         if (isInt && time > 0)
         {
             Map.mapInstance.metadata.timeToFinish = time;
         }
-
-
     }
-    
-    /// <summary>
-    /// Listener for camera movement
-    /// </summary>
-    ///
-    /// Listen for:
-    /// <list type="bullet">
-    ///    <item>Middle click for rotation and zoom</item>
-    ///    <item>Right click for movement</item>
-    /// </list>
-    ///
-    /// Call either <see cref="MoveCamera"/> or <see cref="PivotCamera"/>
+
     private void CameraControls()
     {
-        float xMovement = Input.GetAxis("Mouse X");
-        float yMovement = Input.GetAxis("Mouse Y");
-        float zMovement = Input.GetAxis("Mouse ScrollWheel");
-
-        if (xMovement != 0)
-        {
-            if (Input.GetMouseButton(1) && Input.GetMouseButton(2))
-            {
-                // do nothing
-            }
-            else if (Input.GetMouseButton(2) == true)
-            {
-                MoveCamera(0, xMovement);
-            }
-            else if (Input.GetMouseButton(1) == true)
-            {
-                PivotCamera(0, xMovement);
-            }
-        }
-
-        if (yMovement != 0)
-        {
-            if (Input.GetMouseButton(1) && Input.GetMouseButton(2))
-            {
-                // do nothing
-            }
-            else if (Input.GetMouseButton(2) == true)
-            {
-                MoveCamera(1, yMovement);
-            }
-            else if (Input.GetMouseButton(1) == true)
-            {
-                PivotCamera(1, yMovement);
-            }
-        }
-
-        if (zMovement != 0)
-        {
-            MoveCamera(2, zMovement);
-        }
+        RotateCamera();
+        MoveCamera();
     }
 
-    /// <summary>
-    /// Move the camera in the specified direction
-    /// </summary>
-    /// <param name="direction">The direction in which to move the camera (0: x, 1: y, 2: z)</param>
-    /// <param name="value">The distance to travel</param>
-    private void MoveCamera(int direction, float value)
+    private void RotateCamera()
     {
-        Vector3 translation = new Vector3();
-        const float sensibilityChanger = 1.62f;
+        float sensibility = 1.0f;
 
-        switch (direction)
-        {
-            case 0:
-                translation = new Vector3(value / sensibilityChanger, 0);
-                break;
-            case 1:
-                translation = new Vector3(0, value / sensibilityChanger);
-                break;
-            case 2:
-                translation = new Vector3(0, 0, value);
-                break;
-        }
+        cameraYaw += Input.GetAxis("Mouse X") * sensibility;
+        cameraPitch -= Input.GetAxis("Mouse Y") * sensibility;
 
-
-        this.fovTransform.Translate(translation);
+        mainCamera.transform.eulerAngles = new Vector3(cameraPitch, cameraYaw, mainCamera.transform.rotation.z);
     }
 
-    /// <summary>
-    /// Rotate the camera in the specified direction
-    /// </summary>
-    /// <param name="direction">The direction in which to rotate the camera (0: y, 1: x)</param>
-    /// <param name="value">The distance to travel</param>
-    private void PivotCamera(int direction, float value)
+    private void MoveCamera()
     {
-        Vector3 axis = Vector3.zero;
-
-        switch (direction)
-        {
-            case 0:
-                axis = Vector3.up;
-                break;
-            case 1:
-                axis = Vector3.right;
-                break;
-        }
-
-        this.fovTransform.RotateAround(this.fovTransform.position, axis, value);
+        float sensibility = 0.5f;
+        
+        mainCamera.transform.Translate(Input.GetAxis("Horizontal editor") * sensibility, 0, Input.GetAxis("Vertical editor") * sensibility);
     }
 }
